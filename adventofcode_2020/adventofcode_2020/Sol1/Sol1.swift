5046dce04755ac8ac9eced8d5391c78581225660
Tomislav Andros <r3r00t3d@gmail.com>
2020-12-15 09:57:37 +0100

    Finished day 1

--- adventofcode_2020/adventofcode_2020/Sol1/Sol1.swift
+++ adventofcode_2020/adventofcode_2020/Sol1/Sol1.swift
@@ -7,12 +7,26 @@ struct Sol1 {
         data1 = FileReader1(filename: "input_p1.txt")
     }
     
-    func solve() {
+    func solve1_1() {
         for i in 0..<data1.dataContents.count - 1 {
             for j in i..<data1.dataContents.count {
                 if data1.dataContents[i] + data1.dataContents[j] == 2020 {
+                    print("num1:\(data1.dataContents[i]), num2:\(data1.dataContents[j])")
                     print(String(data1.dataContents[i] * data1.dataContents[j]))
                 }
+            }
+        }
+    }
+    func solve1_2() {
+        for i in 0..<data1.dataContents.count - 2 {
+            for j in i..<data1.dataContents.count - 1 {
+                for k in j..<data1.dataContents.count {
+                    if data1.dataContents[i] + data1.dataContents[j] + data1.dataContents[k] == 2020 {
+                        print("num1:\(data1.dataContents[i]), num2:\(data1.dataContents[j]), num3:\(data1.dataContents[k])")
+                        print(String(data1.dataContents[i] * data1.dataContents[j] * data1.dataContents[k]))
+                    }
+                }
+                
             }
         }
     }
--- adventofcode_2020/adventofcode_2020/main.swift
+++ adventofcode_2020/adventofcode_2020/main.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 let sol1 = Sol1()
-sol1.solve()
+sol1.solve1_1()
+sol1.solve1_2()
 
